211b1ad5315bc3efcb94eddb2024d7f3f0f35e05
Naman Pandey <2492004namanpandey@gmail.com>
2025-07-13 21:15:08 +0530

    fix: cors issue

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
-  'https://leaderboard-point.vercel.app/',
+  'https://leaderboard-point.vercel.app',
 ];
 
 const corsOptions = {
