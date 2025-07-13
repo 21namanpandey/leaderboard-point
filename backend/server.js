81e27b954e4a629454fcff724a2c9829a2f130cc
Naman Pandey <2492004namanpandey@gmail.com>
2025-07-13 21:04:05 +0530

    chore: add frontend url

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+// server.js
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
@@ -8,8 +9,23 @@ import leaderboardRoutes from "./routes/leaderboardRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  'https://leaderboard-point.vercel.app/',
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+};
+
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Database Connection
